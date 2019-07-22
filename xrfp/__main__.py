a87345af34d46b81f8562d6f35e4d34e0f3ee10d
Dylan Armitage <dylanjarmitage@gmail.com>
2019-07-21 23:17:52 -0700

    Rudementary UI using PyQt5
    It even runs by doing `python -m xrfp` !

---
+++ xrfp/__main__.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+
+from pathlib import Path
+
+from PyQt5 import uic
+from PyQt5.QtWidgets import QApplication
+
+root = Path(__file__).parent
+
+Form, Window = uic.loadUiType(root.joinpath("not_implemented.ui"))
+app = QApplication([])
+window = Window()
+form = Form()
+form.setupUi(window)
+window.show()
+app.exec_()
